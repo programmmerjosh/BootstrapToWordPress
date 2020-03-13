ef542431237114b044dec9438ddeccc3cf803d98
Joshua van Niekerk <josh0792252101@gmail.com>
2020-03-13 19:22:30 +0000

    first commit

---
+++ WordPress content files - preview/page-home.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template name: Home Page
+ */
+
+get_header(); ?>
+
+<?php get_template_part('content','hero'); ?>
+<?php get_template_part('content','optin'); ?>
+<?php get_template_part('content','boost'); ?>
+<?php get_template_part('content','who'); ?>
+<?php get_template_part('content','features'); ?>
+<?php get_template_part('content','project'); ?>
+<?php get_template_part('content','featurette'); ?>
+<?php get_template_part('content','instructor'); ?>
+<?php get_template_part('content','testimonials'); ?>
+
+<?php get_footer(); ?>
